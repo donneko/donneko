e1b85ae096b48eda1021a120b19b4d92a30bf574
donneko <117331095+donneko@users.noreply.github.com>
2026-08-17 22:57:23 +0900

    fix: astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,20 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    redirects: {
+        "/site/main.htm": "/",
+        "/site/about.htm": "/about/",
+        "/site/works.htm": "/works/",
+        "/site/diary.htm": "/diary/",
+        "/site/links.htm": "/links/",
+        "/site/nav.htm": "/",
+        "/site/news.htm": "/",
+        "/site/site-works/default.htm": "/works/",
+        "/site/site-works/work-index.htm": "/works/",
+        "/site/site-works/img-1.htm": "/works/",
+        "/site/site-works/img-2.htm": "/works/",
+        "/site/site-works/img-onigiri.htm": "/works/",
+        "/site/site-works/img-live.htm": "/works/",
+    },
+});
