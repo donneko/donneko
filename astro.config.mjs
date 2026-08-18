55d6051a17ee8de142f5afe578b0d94c179f9139
donneko <117331095+donneko@users.noreply.github.com>
2026-08-19 01:07:59 +0900

    delete: redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://donneko.github.io',
-    redirects: {
-        "/site/main.htm": "/",
-        "/site/about.htm": "/about/",
-        "/site/works.htm": "/works/",
-        "/site/diary.htm": "/diary/",
-        "/site/links.htm": "/links/",
-        "/site/nav.htm": "/",
-        "/site/news.htm": "/",
-        "/site/site-works/default.htm": "/works/",
-        "/site/site-works/work-index.htm": "/works/",
-        "/site/site-works/img-1.htm": "/works/",
-        "/site/site-works/img-2.htm": "/works/",
-        "/site/site-works/img-onigiri.htm": "/works/",
-        "/site/site-works/img-live.htm": "/works/",
-    },
+    site: "https://donneko.github.io",
 });
