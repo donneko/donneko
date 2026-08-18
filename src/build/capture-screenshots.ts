a01ae5c21509528558b483ba634733e6a8d9fa2a
donneko <117331095+donneko@users.noreply.github.com>
2026-08-18 23:20:46 +0900

    delete: スクリーンショット系を消去

--- src/build/capture-screenshots.ts
+++
@@ -1,8 +0,0 @@
-import { siteScreenshot } from "../helper/site-screenshot.js";
-import { projects } from "../data/projects.ts";
-import { fixScreenshotPath } from "../helper/fix-screenshot-path.js";
-
-const screenshots = projects.map(({ hasScreenshot, src }) => {
-    if (hasScreenshot) return siteScreenshot(src, `public/${fixScreenshotPath(src)}`);
-});
-await Promise.all(screenshots);
--- src/helper/fix-screenshot-path.ts
+++
@@ -1,6 +0,0 @@
-export function fixScreenshotPath(src: string) {
-    const url = new URL(src);
-    const fixUrl = `${url.hostname}${url.pathname}`.replaceAll(".", "-").replaceAll("/", "-");
-
-    return `screenshot/${fixUrl}.jpeg`;
-}
